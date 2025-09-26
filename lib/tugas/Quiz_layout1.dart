8e08f8a6ae84cf1d0a79562f57c2eb2febe2dac7
rfadwutm <rifautami191@gmail.com>
2025-09-26 18:44:05 +0700

    update

--- lib/tugas/Quiz_layout1.dart
+++ lib/tugas/Quiz_layout1.dart
@@ -4,73 +4,97 @@ class QuizLayout1 extends StatelessWidget {
   const QuizLayout1({super.key});
 
   @override
-Widget build(BuildContext context) {
-  return Scaffold(
-    body: Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [Text("Training",style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold)),
-              IconButton(
-                icon: Icon(Icons.calendar_month),
-                onPressed: () {},
-              ),
-            ],
-          ),
-
-          SizedBox(height: 20),
-
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-                Text("Your Program", style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
-                Text("Details >", style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold, color: Colors.blue)),
-              ],
-          ),
-
-          SizedBox(height: 20),
-
-          Container(
-            width: double.infinity,
-            height: 300,
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  Colors.purple,
-                  Color.fromARGB(41, 184, 0, 216)],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-              ),
-                borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(35),
-                topRight: Radius.circular(200),
-                bottomLeft: Radius.circular(35),
-                bottomRight: Radius.circular(35),
-              ),
-
-            child: Column(
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 60, vertical: 20),
+        child: Column(
+          children: [
+            Row(
               crossAxisAlignment: CrossAxisAlignment.start,
-              children: [ Text("Next Workout",
-                        style: TextStyle(color: Colors.white70, fontSize: 14)),
-                    const SizedBox(height: 8),
-                    const Text(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Training",
+                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                ),
+                Row(
+                  children: const [
+                    Icon(Icons.chevron_left),
+                    Icon(Icons.calendar_month, size: 20),
+                    Icon(Icons.chevron_right),
+                  ],
+                ),
+              ],
+            ),
+
+            const SizedBox(height: 20),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text(
+                  "Your Program",
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "Details >",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue,
+                  ),
+                ),
+              ],
+            ),
+
+            SizedBox(height: 20),
+
+            Container(
+              width: double.infinity,
+              height: 250,
+              decoration: BoxDecoration(
+                gradient: const LinearGradient(
+                  colors: [
+                    Colors.purple,
+                    Color.fromARGB(41, 184, 0, 216),
+                  ],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(35),
+                  topRight: Radius.circular(200),
+                  bottomLeft: Radius.circular(35),
+                  bottomRight: Radius.circular(35),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      "Next Workout",
+                      style: TextStyle(color: Colors.white70, fontSize: 14),
+                    ),
+                    SizedBox(height: 8),
+                    Text(
                       "Lets Toning\nand Glutes Workout",
                       style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold),
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
                 ),
-
-              ],
-            )
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
-    ),
-  );
-}
+    );
+  }
 }
